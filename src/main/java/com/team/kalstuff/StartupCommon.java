381d3600f32bcd2c32f4e46b1093a50a1d09c252
Kalman98 <kalman98k@gmail.com>
2015-04-11 18:23:04 -0500

    Removing dos comments.

--- src/main/java/com/team/kalstuff/StartupCommon.java
+++ src/main/java/com/team/kalstuff/StartupCommon.java
@@ -14,7 +14,6 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 public class StartupCommon
 {
 	
-	//Dis is da' comment dat makes all da' difference.
 
     public static BlockBridge blockBridge;
     public static BlockSquidMat blockSquidMat;
@@ -47,7 +46,6 @@ public class StartupCommon
     	 itemCoffeeMug = new Item().setUnlocalizedName("itemCoffeeMug").setCreativeTab(kalStuffTab);
     	 itemTea = (ItemTea) ((new ItemTea(2, 2.0f, false)).setAlwaysEdible().setUnlocalizedName("itemTea"));
     	 
-    	 //This is da' oda comment dat' makes more o' da' differ'
     	 GameRegistry.registerItem(itemEnderPowder, "itemEnderPowder");
     	 GameRegistry.registerItem(itemCoffee, "itemCoffee");
     	 GameRegistry.registerItem(itemCoffeeMug, "itemCoffeeMug");
