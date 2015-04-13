8e5ec69c37c2bc49d0d5bdae433a8237cdd6c7ce
ploquel <jholley373@yahoo.com>
2015-04-13 11:49:03 -0500

    Added Saving support for the mod config gui.
    The mod gui should now be working 100%. If anyone finds any bugs in
    it, please comment.

--- src/main/java/com/team/KalStuff.java
+++ src/main/java/com/team/KalStuff.java
@@ -3,9 +3,13 @@ package com.team;
 import static net.minecraftforge.common.config.Configuration.CATEGORY_GENERAL;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.common.config.Property;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent.OnConfigChangedEvent;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -13,6 +17,7 @@ import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Mod(modid = KalStuff.MODID, version = KalStuff.VERSION, guiFactory = "com.team.kalstuff.KalStuffGuiFactory")
 public class KalStuff 
@@ -38,9 +43,17 @@ public class KalStuff
         syncConfig(true);
 	}
 
-	private static void syncConfig(boolean load)
+	/**
+	 * reads the config, saves it, and is the blueprint for creating
+	 * the config for the first time
+	 * 
+	 * @author Joseph
+	 */
+	public static void syncConfig(boolean load)
     {
-        if (!config.isChild)
+		List<String> propOrder = new ArrayList<String>();
+		
+		if (!config.isChild)
         {
             if (load)
             {
@@ -50,10 +63,16 @@ public class KalStuff
         
         Property prop;
         
+        //if you want to add new properties, copy this block and change the names
         prop = config.get(CATEGORY_GENERAL, "test", true);
         prop.comment = "for testing this config file";
         prop.setLanguageKey("kalstuff.configgui.test");
+        propOrder.add(prop.getName());
+        
+        
         System.out.println("config value is: " + prop.getBoolean(true));
+        
+        config.setCategoryPropertyOrder(CATEGORY_GENERAL, propOrder);
         
         if (config.hasChanged())
         {
@@ -66,9 +85,27 @@ public class KalStuff
         return config;
     }
 	
+	/**
+	 * called when the config needs to be saved from the config gui
+	 * 
+	 * @author Joseph
+	 */
+	@SubscribeEvent
+    public void onConfigChanged(OnConfigChangedEvent event)
+    {
+		if (MODID.equals(event.modID) && !event.isWorldRunning)
+        {
+            if (Configuration.CATEGORY_GENERAL.equals(event.configID))
+            {
+                syncConfig(false);
+            }
+        }
+    }
+	
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event)
 	{
+		FMLCommonHandler.instance().bus().register(this);
 		proxy.preInit();
 	}
 	
