9984a894c53c0ed681d707ba315504a37da0bcc8
ploquel <jholley373@yahoo.com>
2015-04-17 13:05:29 -0500

    Forgot to uncomment the recipe
    -Also added the Block of Blaze in en_US.lang and en_UK.lang. I dont know
    the other languages (I dont even know what language they are) so someone
    else who does will have to add it in there.

--- src/main/java/com/team/kalstuff/KalStuffRecipes.java
+++ src/main/java/com/team/kalstuff/KalStuffRecipes.java
@@ -53,15 +53,12 @@ public class KalStuffRecipes {
 	 'P', Items.ender_pearl
  });
 
- /*
-  *  GameRegistry.addRecipe(new ItemStack(StartupCommon.blockBlaze, 1), new Object[] {
+ GameRegistry.addRecipe(new ItemStack(StartupCommon.blockBlaze, 1), new Object[] {
 	 "PPP",
 	 "PPP",
 	 "PPP",
 	 'P', Items.blaze_rod
  });
-
-  */
 
 	}
 	
