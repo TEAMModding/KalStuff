7e45cae5fd05bf432e1cd3d454fcd137ac4bced9
CircuitLord <monke_000@Jordan-Acer.home>
2015-04-20 15:34:37 -0500

    Fixing my name.

--- src/main/java/com/team/kalstuff/block/BlockCarrot.java
+++ src/main/java/com/team/kalstuff/block/BlockCarrot.java
@@ -10,7 +10,7 @@ public class BlockCarrot extends Block {
 	public BlockCarrot() {
 		super(Material.grass);
 		this.setCreativeTab(StartupCommon.kalStuffTab);
-		this.setHardness(1.0F);
+		this.setHardness(2.0F);
 	}
 	
 	
