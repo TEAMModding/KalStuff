d3dd8200b6eb667dee9a54469ea28fcb0293d366
CircuitLord <circuitlordmc@gmail.com>
2015-04-23 15:38:51 -0500

    Added blaze soup

---
+++ src/main/java/com/team/kalstuff/item/ItemBlazeSoup.java
@@ -0,0 +1,48 @@
+package com.team.kalstuff.item;
+
+import com.team.kalstuff.StartupCommon;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.world.World;
+
+public class ItemBlazeSoup extends ItemDrink {
+
+	int potionLength = 0;
+	
+	/**
+     * 
+     *  
+     * @param potionLength is the length of the speed effect the player gets upon drinking.
+     */
+	public ItemBlazeSoup(int amount, float saturation, boolean isWolfFood, int potionLength) {
+		super(amount, saturation, isWolfFood);
+		this.setMaxStackSize(1);
+		this.potionLength = potionLength;
+	}
+	
+	public ItemBlazeSoup(int amount, float saturation, boolean isWolfFood) {
+		super(amount, saturation, isWolfFood);
+		this.setMaxStackSize(1);
+	}
+	
+	protected void onFoodEaten(ItemStack stack, World worldIn, EntityPlayer player)
+    {
+		if (!worldIn.isRemote && this.potionLength != 0)
+		{
+			player.addPotionEffect(new PotionEffect(Potion.moveSpeed.id, 6000, 0));
+			player.addPotionEffect(new PotionEffect(Potion.absorption.id, 6000, 0));
+			player.addPotionEffect(new PotionEffect(Potion.saturation.id, 6000, 0));
+			player.addPotionEffect(new PotionEffect(Potion.regeneration.id, 6000, 0));
+			player.addPotionEffect(new PotionEffect(Potion.fireResistance.id, 6000, 0));
+		}
+    }
+    
+	public ItemStack onItemUseFinish(ItemStack stack, World worldIn, EntityPlayer playerIn)
+	  {
+	      super.onItemUseFinish(stack, worldIn, playerIn);
+	      return new ItemStack(StartupCommon.itemGoldenMug);
+	  }
+}
