391ec0e7536d4f97cc3639ebe430081f85b0dc2f
Kalman98 <kalman98k@gmail.com>
2015-04-23 21:01:26 -0500

    Added the other half of the Chicken Nest code. For some reason, Shift + Right clicking chickens out of nests creates a graphical ghost glitch.

--- src/main/java/com/team/kalstuff/tileentity/TileEntityChickenNest.java
+++ src/main/java/com/team/kalstuff/tileentity/TileEntityChickenNest.java
@@ -1,40 +1,100 @@
 package com.team.kalstuff.tileentity;
 
+import net.minecraft.client.renderer.EntityRenderer;
+import net.minecraft.command.IEntitySelector;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
+import net.minecraft.server.gui.IUpdatePlayerListBox;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.ChatComponentTranslation;
 import net.minecraft.util.IChatComponent;
+import net.minecraft.world.World;
+import net.minecraft.entity.passive.EntityChicken;
 
 import java.util.Arrays;
+import java.util.List;
 
-/**
- * User: brandon3055
- * Date: 06/01/2015
- *
- * This is a simple tile entity implementing IInventory that can store 9 item stacks
- */
-public class TileEntityChickenNest extends TileEntity implements IInventory {
+
+public class TileEntityChickenNest extends TileEntity implements IInventory, IUpdatePlayerListBox {
 	// Create and initialize the items variable that will store store the items
-	final int NUMBER_OF_SLOTS = 1;
-	private ItemStack[] itemStacks = new ItemStack[NUMBER_OF_SLOTS];
+	private ItemStack[] inventory = new ItemStack[1];
+	private EntityChicken chicken;
+	private int cooldown;
+	
+	//Check for items
+	@Override
+	public void update() {
+	EntityItem item = CheckForItemEntities(this.worldObj, this.pos.getX(), this.pos.getY(), this.pos.getZ());
+	if (item != null)
+	{
+	ItemStack itemstack = item.getEntityItem().copy();
+	item.setEntityItemStack(this.add(itemstack));
+	System.out.println(itemstack.getItem());
+	}
+		if (this.cooldown > 0) this.cooldown --;
+		if (this.chicken != null && this.chicken.isDead) this.chicken = null;
+		if (this.chicken == null && this.cooldown <= 0) this.chicken = CheckForChickens(this.worldObj, this.pos.getX(), this.pos.getY(), this.pos.getZ());
+		else if (this.chicken != null) {
+			this.chicken.setPosition(this.getPos().getX() + .5, this.getPos().getY(), this.getPos().getZ() + .5);
+	}
 
-	/* The following are some IInventory methods you are required to override */
+	}
 
+	//Find a "random" item over the block
+	public static EntityItem CheckForItemEntities(World worldIn, double p_145897_1_, double p_145897_3_, double p_145897_5_)
+	{
+	List list = worldIn.getEntitiesWithinAABB(EntityItem.class, new AxisAlignedBB(p_145897_1_, p_145897_3_, p_145897_5_, p_145897_1_ + 1.0D, p_145897_3_ + 0.5D, p_145897_5_ + 1.0D), IEntitySelector.selectAnything);
+	return list.size() > 0 ? (EntityItem)list.get(0) : null;
+	}
+	
+	//Find a "random" item over the block
+	public static EntityChicken CheckForChickens(World worldIn, double p_145897_1_, double p_145897_3_, double p_145897_5_)
+	{
+	List list = worldIn.getEntitiesWithinAABB(EntityChicken.class, new AxisAlignedBB(p_145897_1_, p_145897_3_, p_145897_5_, p_145897_1_ + 1.0D, p_145897_3_ + 1.0D, p_145897_5_ + 1.0D), IEntitySelector.selectAnything);
+	return list.size() > 0 ? (EntityChicken)list.get(0) : null;
+	}
+	
+	//Checks if there is any space whatsoever, include partial stacks
+    private boolean hasEmptySpace()
+    {
+    	return (this.inventory[0] == null || this.inventory[0].stackSize != this.inventory[0].getMaxStackSize());
+    }
+	
+    //Checks if there is any space that the specified item can fit in. Meaning only partial stacks if they're the same item.
+    private boolean hasRoomForItem(ItemStack itemstack)
+    {
+    	return (this.inventory[0] == null || (this.inventory[0].stackSize != this.inventory[0].getMaxStackSize() && itemstack.getItem() == inventory[0].getItem()));
+    }
+    
+    //Adds as many items from the item stack as it can to the inventory
+    private ItemStack add(ItemStack itemstack) {
+    	
+    	int i;
+    	for (i = 0; i < itemstack.stackSize && this.hasRoomForItem(itemstack); i ++) {
+        	if (this.inventory[0] == null) this.inventory[0] = new ItemStack(itemstack.getItem(), 1);
+        	else this.inventory[0].stackSize++;
+    		itemstack.stackSize--;
+    	}
+    	return itemstack;
+    }
+    
 	// Gets the number of slots in the inventory
 	@Override
 	public int getSizeInventory() {
-		return itemStacks.length;
+		return inventory.length;
 	}
 
 	// Gets the stack in the given slot
 	@Override
 	public ItemStack getStackInSlot(int slotIndex) {
-		return itemStacks[slotIndex];
+		return inventory[slotIndex];
 	}
 
 	/**
@@ -65,7 +125,7 @@ public class TileEntityChickenNest extends TileEntity implements IInventory {
 	// overwrites the stack in the given slotIndex with the given stack
 	@Override
 	public void setInventorySlotContents(int slotIndex, ItemStack itemstack) {
-		itemStacks[slotIndex] = itemstack;
+		inventory[slotIndex] = itemstack;
 		if (itemstack != null && itemstack.stackSize > getInventoryStackLimit()) {
 			itemstack.stackSize = getInventoryStackLimit();
 		}
@@ -98,7 +158,8 @@ public class TileEntityChickenNest extends TileEntity implements IInventory {
 	// inserting items via the gui
 	@Override
 	public boolean isItemValidForSlot(int slotIndex, ItemStack itemstack) {
-		return true;
+		
+		return itemstack == new ItemStack(Items.egg);
 	}
 
 	// This is where you save any data that you don't want to lose when the tile entity unloads
@@ -113,11 +174,11 @@ public class TileEntityChickenNest extends TileEntity implements IInventory {
 		//   as slot=1, id=2353, count=1, etc
 		// Each of these NBTTagCompound are then inserted into NBTTagList, which is similar to an array.
 		NBTTagList dataForAllSlots = new NBTTagList();
-		for (int i = 0; i < this.itemStacks.length; ++i) {
-			if (this.itemStacks[i] != null)	{
+		for (int i = 0; i < this.inventory.length; ++i) {
+			if (this.inventory[i] != null)	{
 				NBTTagCompound dataForThisSlot = new NBTTagCompound();
 				dataForThisSlot.setByte("Slot", (byte) i);
-				this.itemStacks[i].writeToNBT(dataForThisSlot);
+				this.inventory[i].writeToNBT(dataForThisSlot);
 				dataForAllSlots.appendTag(dataForThisSlot);
 			}
 		}
@@ -133,13 +194,13 @@ public class TileEntityChickenNest extends TileEntity implements IInventory {
 		final byte NBT_TYPE_COMPOUND = 10;       // See NBTBase.createNewByType() for a listing
 		NBTTagList dataForAllSlots = parentNBTTagCompound.getTagList("Items", NBT_TYPE_COMPOUND);
 
-		Arrays.fill(itemStacks, null);           // set all slots to empty
+		Arrays.fill(inventory, null);           // set all slots to empty
 		for (int i = 0; i < dataForAllSlots.tagCount(); ++i) {
 			NBTTagCompound dataForOneSlot = dataForAllSlots.getCompoundTagAt(i);
 			int slotIndex = dataForOneSlot.getByte("Slot") & 255;
 
-			if (slotIndex >= 0 && slotIndex < this.itemStacks.length) {
-				this.itemStacks[slotIndex] = ItemStack.loadItemStackFromNBT(dataForOneSlot);
+			if (slotIndex >= 0 && slotIndex < this.inventory.length) {
+				this.inventory[slotIndex] = ItemStack.loadItemStackFromNBT(dataForOneSlot);
 			}
 		}
 	}
@@ -147,7 +208,7 @@ public class TileEntityChickenNest extends TileEntity implements IInventory {
 	// set all slots to empty
 	@Override
 	public void clear() {
-		Arrays.fill(itemStacks, null);
+		Arrays.fill(inventory, null);
 	}
 
 	// will add a key for this container to the lang file so we can name it in the GUI
@@ -167,6 +228,17 @@ public class TileEntityChickenNest extends TileEntity implements IInventory {
 		return this.hasCustomName() ? new ChatComponentText(this.getName()) : new ChatComponentTranslation(this.getName());
 	}
 
+	public void dropChicken() {
+		this.resetCooldown();
+		if (this.chicken != null) this.chicken.setPositionAndUpdate(this.getPos().getX() + .5, this.getPos().getY() + .5, this.getPos().getZ() + .5);
+		this.chicken = null;
+		System.out.println("Chicken Dropped. Cooldown: " + this.cooldown);
+	}
+	
+	public void resetCooldown() {
+		this.cooldown = 100;
+	}
+	
 	// -----------------------------------------------------------------------------------------------------------
 	// The following methods are not needed for this example but are part of IInventory so they must be implemented
 
