9934352a607d358e643dad46078e437d7b2d22c8
Kalman98 <kalman98k@gmail.com>
2015-05-10 23:20:02 -0500

    Made Walking Stick do 2 attack damage (1 heart). Awesome model coming soon.

--- src/main/java/com/team/kalstuff/item/ItemWalkingStick.java
+++ src/main/java/com/team/kalstuff/item/ItemWalkingStick.java
@@ -1,16 +1,34 @@
 package com.team.kalstuff.item;
 
+import com.google.common.collect.Multimap;
+
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
 public class ItemWalkingStick extends Item
 {
-
+	private float attackDamage;
+	public ItemWalkingStick() {
+		this.attackDamage = 2.0f;
+	}
 	@Override
 	public void onUpdate(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
 		
 
 	}
+	
+    /**
+     * Gets a map of item attribute modifiers, used by ItemSword to increase hit damage.
+     */
+    @SuppressWarnings({ "rawtypes", "unchecked", "deprecation" })
+	public Multimap getItemAttributeModifiers()
+    {
+		Multimap multimap = super.getItemAttributeModifiers();
+        multimap.put(SharedMonsterAttributes.attackDamage.getAttributeUnlocalizedName(), new AttributeModifier(itemModifierUUID, "Weapon modifier", (double)this.attackDamage, 0));
+        return multimap;
+    }
 }
