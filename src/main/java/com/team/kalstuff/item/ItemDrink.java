22d63efd22a452df4d56cc7e0c0d87fec833b5be
Kalvin <kalman98k@gmail.com>
2016-06-08 00:21:57 -0500

    Updated ItemDrink, ItemCoffee, and ItemJewelSoup.

--- src/main/java/com/team/kalstuff/item/ItemDrink.java
+++ src/main/java/com/team/kalstuff/item/ItemDrink.java
@@ -4,35 +4,31 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumAction;
 import net.minecraft.item.ItemFood;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
 import net.minecraft.world.World;
 
 public class ItemDrink extends ItemFood {
 
-    protected boolean alwaysEdible;
-    
+	protected boolean alwaysEdible;
+
 	public ItemDrink(int amount, float saturation, boolean isWolfFood) {
 		super(amount, saturation, isWolfFood);
 	}
-	
-	
-    public ItemStack onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer playerIn)
-    {
-        if (playerIn.canEat(this.alwaysEdible))
-        {
-            playerIn.setItemInUse(itemStackIn, this.getMaxItemUseDuration(itemStackIn));
-        }
 
-        return itemStackIn;
-    }
-    
-    public ItemFood setAlwaysEdible()
-    {
-        this.alwaysEdible = true;
-        return this;
-    }
+	public ActionResult<ItemStack> onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer playerIn,
+			EnumHand hand) {
+		playerIn.setActiveHand(hand);
+		return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, itemStackIn);
+	}
 
-    public EnumAction getItemUseAction(ItemStack stack)
-    {
-        return EnumAction.DRINK;
-    }
+	public ItemFood setAlwaysEdible() {
+		this.alwaysEdible = true;
+		return this;
+	}
+
+	public EnumAction getItemUseAction(ItemStack stack) {
+		return EnumAction.DRINK;
+	}
 }
