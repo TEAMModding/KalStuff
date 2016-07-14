17ba7d7784485e58d6647852c33d919646571961
Kalvin Krajca <kalman98k@gmail.com>
2016-07-14 09:02:02 -0500

    Updated to the versioning method shown here:
    http://mcforge.readthedocs.io/en/latest/conventions/versioning/
    
    Also added accepted Minecraft versions.

--- src/main/java/com/team/KalStuff.java
+++ src/main/java/com/team/KalStuff.java
@@ -23,12 +23,13 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 @Mod(	modid = KalStuff.MODID,
 		name = KalStuff.NAME,
 		version = KalStuff.VERSION,
+		acceptedMinecraftVersions = "[1.9, 1.9.1, 1.9.2, 1.9.3, 1.9.4, 1.10, 1.10.1, 1.10.2]",
 		guiFactory = "com.team.kalstuff.KalStuffGuiFactory")
 public class KalStuff 
 {
 	public static final String MODID = "kalstuff";
 	public static final String NAME = "KalStuff";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "0.7.0";
 	public static int cottageRarity;
 	private static Configuration config;
 	
@@ -58,7 +59,7 @@ public class KalStuff
     {
 		List<String> propOrder = new ArrayList<String>();
 		
-		if (!config.isChild)
+		if (!config.isChild)http://mcforge.readthedocs.io/en/latest/conventions/versioning/
         {
             if (load)
             {
