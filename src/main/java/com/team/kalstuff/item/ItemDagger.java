4096334728f6201b7283c363ab25589745633ebb
Kalvin Krajca <kalman98k@gmail.com>
2016-07-16 01:18:44 -0500

    Made daggers weapons. Also started working on special feature for them.
    Since daggers are weapons they now also take damage and can be
    enchanted. Hooray!

--- src/main/java/com/team/kalstuff/item/ItemDagger.java
+++ src/main/java/com/team/kalstuff/item/ItemDagger.java
@@ -2,33 +2,43 @@ package com.team.kalstuff.item;
 
 import com.google.common.collect.Multimap;
 
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemSword;
 
-public class ItemDagger extends Item {
+public class ItemDagger extends ItemSword {
 
     private final float attackDamage;
 	
-	public ItemDagger(ToolMaterial material) {
-        this.maxStackSize = 1;
+	public ItemDagger(Item.ToolMaterial material) {
+		super(material);
+		this.maxStackSize = 1;
         this.setMaxDamage(material.getMaxUses());
         this.attackDamage = 1 + (material.getDamageVsEntity() / 2);
 	}
-
-	@SuppressWarnings("deprecation")
+	
 	@Override
-	public Multimap<String, AttributeModifier> getItemAttributeModifiers(EntityEquipmentSlot equipmentSlot)
-    {
-        Multimap<String, AttributeModifier> multimap = super.getItemAttributeModifiers(equipmentSlot);
+	public Multimap<String, AttributeModifier> getAttributeModifiers(EntityEquipmentSlot slot, ItemStack stack) {
+		  Multimap<String, AttributeModifier> multimap = super.getItemAttributeModifiers(slot);
 
-        if (equipmentSlot == EntityEquipmentSlot.MAINHAND)
-        {
-            multimap.put(SharedMonsterAttributes.ATTACK_DAMAGE.getAttributeUnlocalizedName(), new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Weapon modifier", (double)this.attackDamage, 0));
-            multimap.put(SharedMonsterAttributes.ATTACK_SPEED.getAttributeUnlocalizedName(), new AttributeModifier(ATTACK_SPEED_MODIFIER, "Weapon modifier", -1.2D, 0));
-        }
+	        if (slot == EntityEquipmentSlot.MAINHAND)
+	        {
+	        	multimap.removeAll(SharedMonsterAttributes.ATTACK_DAMAGE.getAttributeUnlocalizedName());
+	        	multimap.removeAll(SharedMonsterAttributes.ATTACK_SPEED.getAttributeUnlocalizedName());
+	            multimap.put(SharedMonsterAttributes.ATTACK_DAMAGE.getAttributeUnlocalizedName(), new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Weapon modifier", (double)this.attackDamage, 0));
+	            multimap.put(SharedMonsterAttributes.ATTACK_SPEED.getAttributeUnlocalizedName(), new AttributeModifier(ATTACK_SPEED_MODIFIER, "Weapon modifier", -1.2D, 0));
+	        }
 
-        return multimap;
-    }
+	        return multimap;
+	}
+	
+	@Override
+	public boolean hitEntity(ItemStack stack, EntityLivingBase target, EntityLivingBase attacker) {
+		if (target.getHorizontalFacing() == attacker.getHorizontalFacing()) attacker.attackEntityAsMob(target);
+		return super.hitEntity(stack, target, attacker);
+	}
 }
