4e592d212ff5513019cc667b2c2093e925a00205
Kalvin Krajca <kalman98@kalman98.net>
2016-10-23 11:07:32 -0500

    Made boxing gloves work.

--- src/main/java/com/team/kalstuff/item/ItemBoxingGlove.java
+++ src/main/java/com/team/kalstuff/item/ItemBoxingGlove.java
@@ -3,16 +3,32 @@ package com.team.kalstuff.item;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.MobEffects;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.world.WorldServer;
 
 public class ItemBoxingGlove extends Item {
 	
 	@Override
 	public boolean onLeftClickEntity(ItemStack stack, EntityPlayer player, Entity entity) {
 		
-		if (((EntityLivingBase) entity).getHealth() - 1.0F <= 0.0F) return true;
-		else return false;
-	}
-	
+		
+		if (player.getEntityWorld().isRemote) return true;
+		
+		if (((EntityLivingBase) entity).getHealth() - 1.0 <= 0.0F) {
+
+            ((WorldServer)player.getEntityWorld()).spawnParticle(EnumParticleTypes.CRIT, entity.posX, entity.posY + (double)entity.height * 0.5D, entity.posZ, 100, 0.0D, 0.0D, 0.0D, 0.5D, new int[0]);
+
+			((EntityLivingBase) entity).addPotionEffect(new PotionEffect(MobEffects.BLINDNESS, 200, 0));
+			((EntityLivingBase) entity).addPotionEffect(new PotionEffect(MobEffects.SLOWNESS, 200, 9));
+			((EntityLivingBase) entity).addPotionEffect(new PotionEffect(MobEffects.JUMP_BOOST, 200, 128));
+
+			return true;
+		}
+		
+		return false;
+	}	
 }
