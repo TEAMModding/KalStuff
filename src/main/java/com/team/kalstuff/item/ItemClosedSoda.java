8128a6f9cb2cb393f0b9630d1ed90c549f3e634f
pianoman373 <jholley373@yahoo.com>
2016-12-07 21:16:58 -0600

    Update ItemClosedSoda.

--- src/main/java/com/team/kalstuff/item/ItemClosedSoda.java
+++ src/main/java/com/team/kalstuff/item/ItemClosedSoda.java
@@ -21,16 +21,23 @@ public class ItemClosedSoda extends Item {
 	}
 	
 	@Override
-	public ActionResult<ItemStack> onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer playerIn, EnumHand hand) {
-		super.onItemRightClick(itemStackIn, worldIn, playerIn, hand);
+	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand hand) {
+		super.onItemRightClick(worldIn, playerIn, hand);
 		
 		worldIn.playSound(playerIn, playerIn.posX, playerIn.posY, playerIn.posZ, KalStuffSoundEvents.CAN_OPEN, SoundCategory.PLAYERS, 1.0F, 1.0F);
 		
-		--itemStackIn.stackSize;
-        if (itemStackIn.stackSize <= 0) return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, new ItemStack(this.item));
+		ItemStack stack = playerIn.getHeldItem(hand);
+		
+		//TODO: these functions are going to change
+		
+		//pseudocode: stack.addAmount(-1);
+		stack.func_190917_f(-1);
+		
+		//pseudocode: stack.getAmount() <= 0
+        if (stack.func_190916_E() <= 0) return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, new ItemStack(this.item));
         playerIn.inventory.addItemStackToInventory(new ItemStack(this.item));
         
-		return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, itemStackIn);
+		return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, stack);
 	}
 	
 }
