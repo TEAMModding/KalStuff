57f1e4576e20f72052ac5dd89c34900c34c70257
Kalvin Krajca <kalman98@kalman98.net>
2018-04-02 16:40:30 -0500

    Fixed some bugs with boxing glove right click action.

--- src/main/java/com/team/kalstuff/item/ItemBoxingGlove.java
+++ src/main/java/com/team/kalstuff/item/ItemBoxingGlove.java
@@ -5,12 +5,10 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.MobEffects;
-import net.minecraft.init.SoundEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.ActionResult;
-import net.minecraft.util.DamageSource;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.NonNullList;
@@ -54,27 +52,18 @@ public class ItemBoxingGlove extends ItemKalStuff
 	}
 
 	@Override
-	public boolean itemInteractionForEntity(ItemStack stack, EntityPlayer entityplayer, EntityLivingBase target,
+	public boolean itemInteractionForEntity(ItemStack stack, EntityPlayer player, EntityLivingBase target,
 			EnumHand hand)
 	{
-		if (entityplayer.getEntityWorld().isRemote)
-			return super.itemInteractionForEntity(stack, entityplayer, target, hand);
-		entityplayer.swingArm(hand);
-		
-		if (((EntityLivingBase) target).getHealth() - 1.0 <= 0.0F)
-		{
-			((WorldServer) entityplayer.getEntityWorld()).spawnParticle(EnumParticleTypes.CRIT, target.posX,
-					target.posY + (double) target.height * 0.5D, target.posZ, 100, 0.0D, 0.0D, 0.0D, 0.5D, new int[0]);
+		if (player.getEntityWorld().isRemote)
+			return super.itemInteractionForEntity(stack, player, target, hand);
 
-			((EntityLivingBase) target).addPotionEffect(new PotionEffect(MobEffects.BLINDNESS, 200, 0, true, true));
-			((EntityLivingBase) target).addPotionEffect(new PotionEffect(MobEffects.SLOWNESS, 200, 9, true, true));
-
-			((EntityLivingBase) target).addPotionEffect(new PotionEffect(MobEffects.JUMP_BOOST, 200, 128, true, true));
-			return super.itemInteractionForEntity(stack, entityplayer, target, hand);
-		}
-        entityplayer.getEntityWorld().playSound((EntityPlayer)null, entityplayer.posX, entityplayer.posY, entityplayer.posZ, SoundEvents.ENTITY_PLAYER_ATTACK_NODAMAGE, entityplayer.getSoundCategory(), 1.0F, 1.0F);
-		target.attackEntityFrom(DamageSource.causePlayerDamage(entityplayer), 1.0F);
-		return super.itemInteractionForEntity(stack, entityplayer, target, hand);
+		player.setHeldItem(EnumHand.OFF_HAND, player.getHeldItemMainhand());
+		player.setHeldItem(EnumHand.MAIN_HAND, stack);
+		player.attackTargetEntityWithCurrentItem(target);
+		player.setHeldItem(EnumHand.MAIN_HAND, player.getHeldItemOffhand());
+		player.setHeldItem(EnumHand.OFF_HAND, stack);
+		return true;
 	}
 
 	/**
